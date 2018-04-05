01ecaf49632663e7c14ea788571d1fc51950fe4d
Roberto Rocha <rrocha.roberto@gmail.com>
2018-04-05 20:14:05 -0300

    Implementando a soma e a média.

--- sourceCode/Lab1Aula11.c
+++ sourceCode/Lab1Aula11.c
@@ -5,19 +5,32 @@ int main() {
 	//ler valores até digitar -1
 	
 	int valor;
+	int media = 0;
+	int quantidade = 0;
+	int soma = 0;//não pode usar o lixo da memória
+//	printf("Resultado da soma: %i\n", soma);
 	
 	while(valor != -1) { //o -1 é um flag - indica uma condição especial
 		printf("Digite um valor (-1 para terminar): ");
 		scanf("%i", &valor);
+		if(valor != -1) {
+			soma += valor; // é a mesma coisa que: soma = soma + valor;
+			quantidade++;
+		}
 	}
-	printf("Terminou a leitura dos números\n");
 
+	if(quantidade != 0) {
+		media = soma / quantidade;
+	} else {
+		printf("Não foi possível calcular a média.\n");
+	}
+
+	printf("Terminou a leitura dos números\n");
+	printf("Resultado da soma: %i\n", soma);
+	printf("Resultado da média: %i\n", media);
 	//Exercício 1: mostrar a soma de todos os números lidos (exceto o -1)
-	
 	//Exercício 2: mostrar a média aritmética dos valores lidos
-	
 	//Exercício 3: mostrar o maior e o menor valor
-	
 	
 	/*
 		Utilizar as seguintes sequências de valores para testar o código:
