e6d59f27251612c125d6a117306acdc2fab21d02
Roberto Rocha <rrocha.roberto@gmail.com>
2018-04-17 21:51:50 -0300

    Melhoramento do MMC

--- sourceCode/mmc.c
+++ sourceCode/mmc.c
@@ -2,19 +2,20 @@
 
 int main() {
 
-	int v1 = 60;
-	int v2 = 90;
-	
+	int v1, v2;
 	int m1 = 0, m2 = 0;
 	
+	printf("Digite um valor: ");
+	scanf("%i", &v1);
+	printf("Digite outro valor: ");
+	scanf("%i", &v2);
+	
+	//método da tabuada
 	while(m1 != m2 || m1 == 0) {
 		if(m1 < m2) {
 			m1 += v1;
 		} else {
 			m2 += v2;
-		}
-		if( m1 == m2) {
-			break;
 		}
 	}
 	printf("Resultado: %i\n", m1);
