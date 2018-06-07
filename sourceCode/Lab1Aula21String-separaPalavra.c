b22f3d2a4f127b3b13c31a2bc3bcfc597a45b508
Roberto Rocha <rrocha.roberto@gmail.com>
2018-06-07 20:14:28 -0300

    Usando um vetor de char como string.

---
+++ sourceCode/Lab1Aula21String-separaPalavra.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int main() {
+	char vetorDeChar[MAX];
+	int i;
+	
+	printf("Digite %i caracteres: ", MAX);
+	
+	//faz a leitura dos caracteres
+	gets(vetorDeChar);
+	
+	printf("Frase lida: %s\n", vetorDeChar);
+	
+	printf("Vai imprimir as palavras separadamente.\n");
+	
+	//imprimir uma palavra por linha
+	for(i = 0; i < MAX; i++) {
+		if(vetorDeChar[i] == '\0') { //chegou no fim da string
+			break;
+		}
+		if(vetorDeChar[i] == ' ') { //se for espaço, imprime \n
+			printf("\n");
+		}
+		printf("%c", vetorDeChar[i]);
+	}
+}
---
+++ sourceCode/Lab1Aula21String.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAX 1000
+
+int main() {
+	char vetorDeChar[MAX];
+	int i;
+	int contLetraA = 0;
+	int contLetraE = 0;
+	int contLetraI = 0;
+	int contLetraO = 0;
+	int contLetraU = 0;
+	int contEspaco = 0;
+	
+	printf("Digite %i caracteres: ", MAX);
+	
+	//faz a leitura dos caracteres
+	gets(vetorDeChar);
+	
+	//contar a quantidade de cada vogal
+	
+	for(i = 0; i < MAX; i++) {
+		switch(vetorDeChar[i]) {
+			case 'a':
+			case 'A':
+				contLetraA++;
+				break;
+			case 'e':
+			case 'E':
+				contLetraE++;
+				break;
+			case 'i':
+			case 'I':
+				contLetraI++;
+				break;
+			case 'o':
+			case 'O':
+				contLetraO++;
+				break;
+			case 'u':
+			case 'U':
+				contLetraU++;
+				break;
+			case ' ':
+				contEspaco++;
+				break;
+		}
+	}
+	printf("Quantidade de a's: %i\n", contLetraA);
+	printf("Quantidade de e's: %i\n", contLetraE);
+	printf("Quantidade de i's: %i\n", contLetraI);
+	printf("Quantidade de o's: %i\n", contLetraO);
+	printf("Quantidade de u's: %i\n", contLetraU);
+	printf("Quantidade de espaços: %i\n", contEspaco);
+}
